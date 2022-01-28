070eb28590d784269711e760e219c55193462ddf
kanishak garg <kanishakgarg8778@gmail.com>
2022-01-28 12:45:52 +0530

    add POST UI

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,12 +2,13 @@ import { UPDATE_POSTS } from './actionTypes';
 
 export function fetchPosts() {
   return (dispatch) => {
-    const url = 'http://codeial.com:8000/api/v2/posts?page=1&limit=5';
+    const url = 'http://localhost:8000/api/v2/posts?page=1&limit=5';
     fetch(url)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
+        console.log('data', data);
         dispatch(updatePosts(data.data.posts));
       });
   };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './components/App';
+import { Provider } from 'react-redux';
+
 import { configureStore } from './store';
 
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
