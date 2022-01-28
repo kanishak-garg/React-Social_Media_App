930639f2c9f03ed05ff287dd40517281573d471d
kanishak garg <kanishakgarg8778@gmail.com>
2022-01-28 13:30:18 +0530

    Design the Posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/posts';
-
+import PostsList from './PostsList';
 class App extends React.Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
   }
 
   render() {
-    return <div>App</div>;
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
   }
 }
 
