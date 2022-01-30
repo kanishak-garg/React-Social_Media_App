c13a990478c2c2eb7d965edfb3d746110dc02e82
kanishak garg <kanishakgarg8778@gmail.com>
2022-01-30 20:27:49 +0530

    add Login form Uncontrolled

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import { connect } from 'react-redux';
 import { BrowserRouter as Router, Routes, Link, Route } from 'react-router-dom';
 import { fetchPosts } from '../actions/posts';
 import propTypes from 'prop-types';
-import { Home, Navbar, Page404 } from './';
+import { Home, Navbar, Page404, Login } from './';
 
-const Login = () => <div>Login</div>;
 const Signup = () => <div>Signup</div>;
 
 class App extends React.Component {
@@ -21,12 +20,7 @@ class App extends React.Component {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home posts={posts} />} />
-            <Route
-              path="/login"
-              render={(props) => {
-                return <Home {...props} posts={posts} />;
-              }}
-            />
+            <Route path="/login" element={<Login />} />;
             <Route path="/signup" element={<Signup />} />
             <Route path="*" element={<Page404 />} />
           </Routes>
--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ import PostsList from './PostsList';
 import Home from './Home';
 import Navbar from './Navbar';
 import Page404 from './Page404';
-export { PostsList, Navbar, Home, Page404 };
+import Login from './Login';
+
+export { PostsList, Navbar, Home, Page404, Login };
